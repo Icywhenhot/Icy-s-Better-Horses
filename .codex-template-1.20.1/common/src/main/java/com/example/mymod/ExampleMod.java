b01bf5258205e70d6a5b12723b596c05fbb49c17
icy <icywhenhot@tutamail.com>
2026-09-17 21:56:44 +0500

    clean out the template dumps, stale notes and debug logs

--- .codex-template-1.20.1/common/src/main/java/com/example/mymod/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package com.example.mymod;
-
-public class ExampleMod {
-    public static final String MOD_ID = "myawesomemod";
-
-    public static void init() {
-        System.out.println("Hello World!");
-    }
-}
